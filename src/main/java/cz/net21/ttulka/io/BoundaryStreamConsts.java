1a8cf9beb260acb03df447a54d7cadae63530eca
Tomas Tulka <tomas.tulka@gmail.com>
2017-11-15 11:31:25 +0100

    SUPER_BOUNDARY default value added

--- src/main/java/cz/net21/ttulka/io/BoundaryStreamConsts.java
+++ src/main/java/cz/net21/ttulka/io/BoundaryStreamConsts.java
@@ -9,6 +9,8 @@ public final class BoundaryStreamConsts {
 
     public static final byte[] BOUNDARY = "-----StreamBoundary-----".getBytes();
 
+    public static final byte[] SUPER_BOUNDARY = "=====StreamBoundary=====".getBytes();
+
     private BoundaryStreamConsts() {
         throw new IllegalStateException("Cannot create an instance of this class.");
     }
